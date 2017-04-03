9a3d39a88f4b83d66633b019cb155ebd46bb01bf
Valdis <valdisxp1@gmail.com>
2017-04-03 22:32:58 +0200

    assert messages

--- macros/src/main/scala/gestalt/macros/Quasiquotes.scala
+++ macros/src/main/scala/gestalt/macros/Quasiquotes.scala
@@ -6,12 +6,12 @@ import scala.gestalt._
 class modsTest extends StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
     val q"$mods object $name" = q"private object A"
-    assert(mods.is(flags.Private))
+    assert(mods.is(flags.Private), "object was not private")
 
     val q"$mods1 class $name2 $mods2 ($params)" = q"case class A private[core](x: Int)"
-    assert(mods1.is(flags.Case))
-    assert(mods2.is(flags.Private))
-    assert(mods2.privateWithin == "core")
+    assert(mods1.is(flags.Case), "class was not case")
+    assert(mods2.is(flags.Private), "class was not private")
+    assert(mods2.privateWithin == "core", s"class private within ${mods.privateWithin}, expected: core ")
 
     defn
   }
