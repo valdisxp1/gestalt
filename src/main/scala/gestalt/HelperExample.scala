11abbe0e027b94b66756ab2492bf2ab18233acd8
Valdis <valdisxp1@gmail.com>
2017-04-10 13:05:53 +0200

    cleanup

--- src/main/scala/gestalt/HelperExample.scala
+++ src/main/scala/gestalt/HelperExample.scala
@@ -2,18 +2,20 @@ package example.helpers
 
 import scala.gestalt.{Toolbox, TypeToolbox}
 
-class HelperExample[T<:Toolbox](val tb: T) {
-  def intArrayLit(array: Int*): tb.Tree = tb.Apply(tb.Ident("Array"),array.toList.map(tb.Lit))
+class HelperExample[T <: Toolbox](val tb: T) {
+  def intArrayLit(array: Int*): tb.Tree = tb.Apply(tb.Ident("Array"), array.toList.map(tb.Lit))
 }
 
 class HelperExample2[T <: TypeToolbox](val tb: T) {
   def intArrayLit(array: Int*): tb.Tree = tb.Apply(tb.Ident("Array"), array.toList.map(tb.Lit))
-  object Varargs{
-    def unapply(tree: tb.Tree): Option[Either[tb.Tree,Seq[tb.Tree]]] = tree match {
+
+  object Varargs {
+    def unapply(tree: tb.Tree): Option[Either[tb.Tree, Seq[tb.Tree]]] = tree match {
       case tb.SeqLiteral(seq) => Some(Right(seq))
-      case tb.Ascribe(seq,_) => Some(Left(seq))
+      case tb.Ascribe(seq, _) => Some(Left(seq))
       case _ => None
     }
   }
+
 }
 
