4de0a13a39d7fdb859f4c1aa5c7b316b72cd8d53
Valdis <valdisxp1@gmail.com>
2017-04-11 13:00:21 +0200

    the setup

---
+++ macros/src/main/scala/gestalt/macros/JsonMacros.scala
@@ -0,0 +1,19 @@
+package gestalt.macros
+
+import scala.gestalt._
+
+object JsonMacros {
+
+  sealed trait JsValue
+  final case class JsString(value: String)
+  final case class JsArray(items: Seq[(String, JsValue)])
+
+  trait Format[A] {
+    def toJson(o: A): JsValue
+    def fromJson(json: JsValue): Option[A]
+  }
+
+  def format[T]() = meta {
+    q"null"
+  }
+}
---
+++ macros/src/test/scala/gestalt/macros/JsonMacroTest.scala
@@ -0,0 +1,8 @@
+import gestalt.macros.JsonMacros
+
+class JsonMacroTest extends TestSuite {
+  test("simple case class"){
+    case class Name(first: String, last: String)
+    val format = JsonMacros.format[Name]()
+  }
+}
