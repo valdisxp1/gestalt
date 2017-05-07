0f70a1d0c03c9137727c5bb23ae51e4a5ef35d0c
Valdis <valdisxp1@gmail.com>
2017-05-07 22:18:11 +0200

    this really breaks the compiler

--- macros/src/test/scala/gestalt/macros/JsonMacroTest.scala
+++ macros/src/test/scala/gestalt/macros/JsonMacroTest.scala
@@ -1,7 +1,28 @@
 class JsonMacroTest extends TestSuite {
   test("simple case class"){
     case class Name(first: String, last: String)
-    val format = JsonMacros.format[Name]()
+    val format = new JsonMacros.Format[Name] {
+      def toJson(o: Name) =
+        JsonMacros.JsObject(
+          Seq("first".->(JsonMacros.JsString(o.first)),
+            "last".->(JsonMacros.JsString(o.last))
+          )
+        )
+
+      def fromJson(json: JsonMacros.JsValue) =
+        json match {
+          case obj: JsonMacros.JsObject =>
+            val first =
+              obj.firstValue("first").collect {
+                case JsonMacros.JsString(value) => value
+              }
+            val last = obj.firstValue("last").collect {
+              case JsonMacros.JsString(value) => value
+            }
+            if (first.isDefined.&&(last.isDefined)) Some(null) else None
+          case other => None
+        }
+    }
     import JsonMacros._
     assert(format.toJson(Name("John","Smith")) == JsObject(Seq(
       "first" -> JsString("John"),
