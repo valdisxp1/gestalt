ef0796ab6e17b9a1c2e41e41df9aa9c3695625d8
Valdis <valdisxp1@gmail.com>
2017-05-07 22:36:17 +0200

    replicated

--- macros/src/test/scala/gestalt/macros/JsonMacroTest.scala
+++ macros/src/test/scala/gestalt/macros/JsonMacroTest.scala
@@ -1,12 +1,15 @@
 class JsonMacroTest extends TestSuite {
   test("simple case class"){
     case class Name(first: String, last: String)
-    import JsonMacros._
 
-    val obj: JsObject = JsObject(Nil)
-    val first =
-      obj.firstValue("first").collect {
-        case JsonMacros.JsString(value) => value
-      }
+    val someBee: Option[Bees.Bee] = Some(Bees.Worker("buzz"))
+    val test = someBee.collect{
+      case Bees.Worker(value) => value
+    }
   }
+}
+
+object Bees{
+  final case class Worker(value: String) extends Bee
+  sealed trait Bee
 }
