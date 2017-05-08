e2102211d43c74ac6f0bf69c858e398f886375c8
Valdis <valdisxp1@gmail.com>
2017-05-08 16:30:55 +0200

    almost extracted

--- macros/src/test/scala/gestalt/macros/BreakTheCompiler.scala
+++ macros/src/test/scala/gestalt/macros/BreakTheCompiler.scala
@@ -1,7 +1,11 @@
-object Main {
-  val buzz = Some(Bees.Bee("buzz")).collect{
-    case Bees.Bee(value) => value
+class Main {
+  runCode("abcde"){
+    case class Name(first: String, last: String)
+    val buzz = Some(Bees.Bee("buzz")).collect{
+      case Bees.Bee(value) => value
+    }
   }
+  def runCode(comment: String)(code: => Unit) = code
 }
 
 object Bees {
--- macros/src/test/scala/gestalt/macros/JsonMacroTest.scala
+++ macros/src/test/scala/gestalt/macros/JsonMacroTest.scala
@@ -1,7 +1,9 @@
 class JsonMacroTest extends TestSuite {
   test("simple case class"){
     case class Name(first: String, last: String)
-
+    val buzz = Some(Bees.Bee("buzz")).collect{
+      case Bees.Bee(value) => value
+    }
 
   }
 }
