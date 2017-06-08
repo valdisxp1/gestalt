3604d7bf1af5cb8f39fd2e169e54b47c926d863e
Valdis <valdisxp1@gmail.com>
2017-06-08 18:58:47 +0300

    typed tuples

--- src/main/scala/gestalt/dotty/DottyToolbox.scala
+++ src/main/scala/gestalt/dotty/DottyToolbox.scala
@@ -661,8 +661,15 @@ class Toolbox(enclosingPosition: Position)(implicit ctx: Context) extends Tbox {
 
   object Tuple extends TupleImpl {
     def apply(args: List[TermTree]): TermTree = d.Tuple(args).withPosition
-    //TODO check
-    def apply(args: List[tpd.Tree])(implicit c: Dummy): tpd.Tree = d.Tuple(args).withPosition
+    def apply(ts: List[tpd.Tree])(implicit c: Dummy): tpd.Tree = {
+        val arity = ts.length
+        def tupleTypeRef: TypeRef = Type.typeRef("scala.Tuple"+arity)
+        /*if (arity > Definitions.MaxTupleArity) {
+          error(TupleTooLong(ts), tree.pos)
+          Definitions.unitLiteral
+        } else */if (arity == 1) ts.head
+        else t.AppliedTypeTree(t.TypeTree(tupleTypeRef), ts).withPosition
+    }
 
     def unapply(tree: Tree): Option[List[TermTree]] = tree match {
       case d.Tuple(trees) => Some(trees)
